7cc7840f330ad8abb5b7357dd4d4b1e5770fdbfe
Ruslan Sotnikov <ruslan.sot70@gmail.com>
2024-11-27 11:20:07 +0300

    fix: change dev logger format to be contain more info

--- config/dev.exs
+++ config/dev.exs
@@ -73,8 +73,9 @@ config :vimperfect, VimperfectWeb.Endpoint,
 # Enable dev routes for dashboard and mailbox
 config :vimperfect, dev_routes: true
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console,
+  format: "[$level] $message | $metadata\n",
+  metadata: [:request_id, :addr, :conn, :chan, :mfa]
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
